dd4157067d85c21d00079e02e6966ee1f9aa6d64
hikon <hikonnico2@gmail.com>
2017-10-19 13:12:43 +0000

    リンク先変更

--- yuhan.js
+++ yuhan.js
@@ -1,44 +1,30 @@
 
 myTable = new Array(	// リンク先の登録
-"http://www.dominos.jp/order/pizza/detail/99999/19001/241",	// ドミノ・ピザ
-"http://www.dominos.jp/order/pizza/detail/99999/19001/238",	
-"http://www.dominos.jp/order/pizza/detail/99999/19001/208",
-"http://www.dominos.jp/order/pizza/detail/99999/19001/531",
-"http://www.dominos.jp/order/pizza/detail/99999/19001/97",
-"http://www.dominos.jp/order/pizza/detail/99999/19001/217",
-"http://www.dominos.jp/order/pizza/detail/99999/19001/95",
-"http://www.dominos.jp/order/pizza/detail/99999/19001/532",
-"http://www.dominos.jp/order/pizza/detail/99999/19001/238",
-"http://www.dominos.jp/order/pizza/detail/99999/19001/129", // ドミノ・ピザここまで
-"https://www.pizza-la.co.jp/Item.aspx?id=0004965&ListId=pizza",	// ピザーラ
-"https://www.pizza-la.co.jp/Item.aspx?id=0005571&ListId=pizza", 
-"https://www.pizza-la.co.jp/Item.aspx?id=0004536&ListId=pizza",
-"https://www.pizza-la.co.jp/Item.aspx?id=0004546&ListId=pizza",
-"https://www.pizza-la.co.jp/Item.aspx?id=0004534&ListId=pizza",
-"https://www.pizza-la.co.jp/Item.aspx?id=0005100&ListId=pizza",
-"https://www.pizza-la.co.jp/Item.aspx?id=0004575&ListId=pizza",
-"https://www.pizza-la.co.jp/Item.aspx?id=0005184&ListId=pizza",
-"https://www.pizza-la.co.jp/Item.aspx?id=0005416&ListId=pizza",
-"https://www.pizza-la.co.jp/Item.aspx?id=0005184&ListId=pizza",	// ピザーラここまで
-"https://pizzahut.jp/pc/pizza/W00001N14V-002",	// ピザハット
-"https://pizzahut.jp/pc/pizza/W00001N11V-002",
-"https://pizzahut.jp/pc/pizza/W000002638-002",
-"https://pizzahut.jp/pc/pizza/W000002878-002",
-"https://pizzahut.jp/pc/pizza/W000002674-002",
-"https://pizzahut.jp/pc/pizza/W000003010-002",
-"https://pizzahut.jp/pc/pizza/W000002378-002",
-"https://pizzahut.jp/pc/pizza/W000002680-002",
-"https://pizzahut.jp/pc/pizza/W000001118-002", // ピザハットここまで
-"http://www.kfc.co.jp/menu/delivery/?menu_id=314", // ケンタッキー
-"http://www.kfc.co.jp/menu/delivery/?menu_id=317",
-"http://www.kfc.co.jp/menu/delivery/?menu_id=271",
-"http://www.kfc.co.jp/menu/delivery/?menu_id=146", //ケンタッキーここまで
-"https://www.ginsara.jp/menu/gin-3_1_detail.html", // 銀のさら
-"https://www.ginsara.jp/menu/odori-3_1_detail.html",
-"https://www.ginsara.jp/menu/mino-3_1_detail.html",
-"https://www.ginsara.jp/menu/mizuho-3_1_detail.html",
-"https://www.ginsara.jp/menu/kaga-3_1_detail.html",
-"https://www.ginsara.jp/menu/sagami-3_1_detail.html" // 銀のさらここまで
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1609",	// ドミノ・ピザ
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1634",	
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1635",
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1604",
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1607",
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1608",
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1617",
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1619",
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1622",
+  "http://www.dominos.jp/order/pizza/detail/99999/19001/1624", // ドミノ・ピザここまで
+  "https://www.pizza-la.co.jp/Item.aspx?id=0006148&ListId=pizza",	// ピザーラ
+  "https://www.pizza-la.co.jp/Item.aspx?id=0006149&ListId=pizza", 
+  "https://www.pizza-la.co.jp/Item.aspx?id=0006150&ListId=pizza",
+  "https://www.pizza-la.co.jp/Item.aspx?id=0006126&ListId=pizza",
+  "https://www.pizza-la.co.jp/Item.aspx?id=0005987&ListId=pizza",
+  "https://www.pizza-la.co.jp/Item.aspx?id=0006062&ListId=pizza",// ピザーラここまで
+  "https://pizzahut.jp/pc/pizza/W000002916-002",	// ピザハット
+  "https://pizzahut.jp/pc/pizza/W000003714-002",
+  "https://pizzahut.jp/pc/pizza/W000003295-002",
+  "https://pizzahut.jp/pc/pizza/W00001N19V-002",
+  "https://pizzahut.jp/pc/pizza/W000002379-002",
+  "https://pizzahut.jp/pc/pizza/W000001611-002",
+  "https://pizzahut.jp/pc/pizza/W000001071-002",
+  "https://pizzahut.jp/pc/pizza/W000002915-002",
+  "https://pizzahut.jp/pc/pizza/W000002674-002", // ピザハットここまで
 );	
 function myRndLink(){	// 適当に選んでリンクする関数
      myRnd = Math.floor( Math.random() * myTable.length );	// 0～(URLの数-1)の乱数を求める
